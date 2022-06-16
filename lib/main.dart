7b3055ad1cea892af2a49fff00aae7d96e43770d
ryanaltair <al_tair@live.com>
2022-06-16 11:37:57 +0800

    add file open

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:file_selector/file_selector.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:flutter/material.dart';
@@ -24,12 +25,27 @@ class _VideoAppState extends State<VideoApp> {
     return MaterialApp(
       title: 'Video Demo',
       home: Scaffold(
-        body: Container(),
+        body: Container(
+          child: TextButton(
+              onPressed: () async {
+                final XTypeGroup typeGroup = XTypeGroup(
+                  label: 'images',
+                  extensions: <String>['mp4'],
+                );
+                final XFile? file =
+                    await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
+                log('file $file');
+                if (file != null) {
+                  launchUrlString('file:${file.path}');
+                }
+              },
+              child: const Text('打开视频文件')),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
             launchUrlString('file:/Users/ryanaltair/Downloads/bee.mp4');
           },
-          child: Icon(
+          child: const Icon(
             Icons.play_arrow,
           ),
         ),
